106cb1da8f868173a7de0a9dacab875727d8c4fb
lucasbaitello <lucasbaitello@gmail.com>
2025-03-04 23:50:38 -0300

    primeira pagina

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 function home() {
   return (
-    <div>
-      <h1>Hodgggggdgdme</h1>
-    </div>
+    <><div>
+          <h1>Hodgggggdgdme</h1>
+      </div><div>
+              <h1>bora ve essa parada ai</h1>
+          </div></>
   );
 }
 
